f54f46e53f35d952d025eb7b6a670800d8df529c
Mythrayee <mythrayee@hotmail.com>
2020-04-30 21:26:15 -0700

    adding files

---
+++ EntityFramework1/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EntityFramework1
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public int Salary { get; set; }
+        public int DepartmentId { get; set; }
+        [ForeignKey("DepartmentId")]
+        public Department Department { get; set; }
+        public string JobTitle { get; set; }
+    }
+}
